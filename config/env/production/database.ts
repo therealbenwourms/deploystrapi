2453af52c9c22c4163d4e87285bce44193909244
therealbenwourms <ben@dreamx.homes>
2025-03-20 20:02:35 +0000

    Updated database configuration and i am ready quit please let me done theres no more anything

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -2,14 +2,15 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT', 25060),
+        database: env('DATABASE_NAME', 'defaultdb'),
+        user: env('DATABASE_USERNAME', 'doadmin'),
+        password: env('DATABASE_PASSWORD', 'AVNS_sa3VAlzH6QMOsIrYXnh'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not required
         ssl: {
-          ca: env('DATABASE_CA')
+          ca: env('DATABASE_CA'),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
         },
       },
       debug: false,
